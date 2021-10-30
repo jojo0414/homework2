a3810795356399cf6e160f17adbc71c50094075a
andycheng <andychz0519@gmail.com>
2021-10-30 23:55:36 +0800

    Initial Commit

---
+++ homework2/homework2App.swift
@@ -0,0 +1,17 @@
+//
+//  homework2App.swift
+//  homework2
+//
+//  Created by User12 on 2021/10/30.
+//
+
+import SwiftUI
+
+@main
+struct homework2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
